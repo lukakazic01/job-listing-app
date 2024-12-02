e711fc411a6bbb34c8fd5598598ca70dbd9225a0
Luka Kazic <lukakazic@Lukas-MacBook-Pro.local>
2024-12-02 19:25:30 +0100

    Added pre commit hook

--- resources/js/Pages/Index.vue
+++ resources/js/Pages/Index.vue
@@ -2,6 +2,10 @@
   <div class="text-red-500">Hello world</div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+const a = ref()
+</script>
 
 <style lang="scss" scoped></style>
