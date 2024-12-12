16277774c070815400c0c7022ee20b5ddb89e554
lukakazic01 <kazicluka4@gmail.com>
2024-12-12 14:58:57 +0100

    small fix

--- app/Http/Controllers/CompanySizeController.php
+++ app/Http/Controllers/CompanySizeController.php
@@ -3,11 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\CompanySize;
-use Illuminate\Http\Request;
 
 class CompanySizeController extends Controller
 {
-    public function __construct() {
+    public function __invoke() {
         return CompanySize::all()->map(fn($companySize) => ['value' => $companySize->id, 'label' => $companySize->name]);
     }
 }
